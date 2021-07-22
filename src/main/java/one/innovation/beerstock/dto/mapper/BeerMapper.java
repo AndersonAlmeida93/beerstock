1fbf44715664319b7cc82ea15a00dad4e25ab4b8
Anderson Guilherme da Silva Almeida <8920@tce.pe>
2021-07-22 10:21:06 -0300

    implementado testes unitarios.

--- src/main/java/one/innovation/beerstock/dto/mapper/BeerMapper.java
+++ src/main/java/one/innovation/beerstock/dto/mapper/BeerMapper.java
@@ -1,6 +1,7 @@
 package one.innovation.beerstock.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import one.innovation.beerstock.dto.BeerDTO;
 import one.innovation.beerstock.entity.Beer;
@@ -8,6 +9,8 @@ import one.innovation.beerstock.entity.Beer;
 @Mapper(componentModel = "spring")
 public interface BeerMapper {
 
+	BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+	  
 	Beer toModel(BeerDTO beerDTO);
 	
 	BeerDTO toDto(Beer beer);
